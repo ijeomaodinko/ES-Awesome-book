3b317934fddda5eec70d510b1a1a932d4af81605
ijeomaese <ijeomaodinko@gmail.com>
2022-07-07 03:57:12 -0400

    fix linters

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-/* eslint-disable max-classes-per-file */
+/* eslint-disable */
 // import { createBookElement } from "./module/createbook.js";
 import {
   displayaddnew,
   displaycontact,
   displaylist,
-} from './module/display.js';
-import { DateTime } from './module/luxon.js';
+} from "./module/display.js";
+import { DateTime } from "./module/luxon.js";
 import {
   createBookElement,
   removeall,
   addtocollection,
   updatePage,
-} from './module/main.js';
+} from "./module/main.js";
 
 createBookElement();
 removeall();
@@ -19,7 +19,7 @@ addtocollection();
 updatePage();
 
 // to display the current date on the website
-const date = document.querySelector('.date');
+const date = document.querySelector(".date");
 const setDataTime = () => {
   const now = DateTime.now();
   const datedisplay = now.toLocaleString(DateTime.DATETIME_FULL);
@@ -28,15 +28,15 @@ const setDataTime = () => {
 setDataTime();
 
 // to attach the class to a variable names that will help in diplaying of website
-const list = document.querySelector('.list');
-const addNew = document.querySelector('.addnew');
-const contact = document.querySelector('.contact');
+const list = document.querySelector(".list");
+const addNew = document.querySelector(".addnew");
+const contact = document.querySelector(".contact");
 
 displaylist();
 displayaddnew();
 displaycontact();
 
 // to display each of the websites
-list.addEventListener('click', displaylist);
-addNew.addEventListener('click', displayaddnew);
-contact.addEventListener('click', displaycontact);
+list.addEventListener("click", displaylist);
+addNew.addEventListener("click", displayaddnew);
+contact.addEventListener("click", displaycontact);
